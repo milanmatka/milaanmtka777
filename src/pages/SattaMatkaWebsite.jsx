81b2b62e88e638e19b1cc810e9707e50551fcc6b
milanmatka <milanmatka777@gmail.com>
2025-06-09 15:10:45 +0530

    Update SattaMatkaWebsite.jsx

--- src/pages/SattaMatkaWebsite.jsx
+++ src/pages/SattaMatkaWebsite.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import Logo from '../assets/Logo.jpeg'
+import Logo from '../assets/newlogo.png'
 
 const SattaMatkaWebsite = () => {
   const [currentTime, setCurrentTime] = useState(new Date());
